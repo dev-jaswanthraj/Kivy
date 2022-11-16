d4baf98b91794d3bd62394cadb299cdcd0676fbe
Jaswanthraj R <jaswanthraj.r@gmail.com>
2022-11-16 13:33:27 +0530

    BoXLayout

--- Basic Example/TheLabApp/main.py
+++ Basic Example/TheLabApp/main.py
@@ -1,5 +1,21 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class BoxLayoutExample(BoxLayout):
+    pass
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+
+    #     self.orientation = "vertical"
+    #     b1 = Button(text = "A")
+    #     b2 = Button(text = "B")
+    #     b3 = Button(text = "C")
+
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    #     self.add_widget(b3)
 
 class MainWidget(Widget):
     pass
