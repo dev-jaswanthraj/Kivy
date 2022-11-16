3ecff225deb74a59dc1e3cdeb39e59f138a0c241
Jaswanthraj R <jaswanthraj.r@gmail.com>
2022-11-16 13:45:55 +0530

    AnchorLayout

--- Basic Example/TheLabApp/main.py
+++ Basic Example/TheLabApp/main.py
@@ -2,7 +2,9 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-
+from kivy.uix.anchorlayout import AnchorLayout
+class AnchorLayoutExample(AnchorLayout):
+    pass
 class BoxLayoutExample(BoxLayout):
     pass
     # def __init__(self, **kwargs):
