559e97b400a3dd74a0fb41b7479b6f5b3c2f177d
Jaswanthraj R <jaswanthraj.r@gmail.com>
2022-11-17 07:14:01 +0530

    ScrollView

--- Basic Example/TheLabApp/main.py
+++ Basic Example/TheLabApp/main.py
@@ -12,7 +12,7 @@ class StackLayoutExample(StackLayout):
         super().__init__(**kwargs)
         # Orientation's must be one of: ['lr-tb', 'tb-lr', 'rl-tb', 'tb-rl', 'lr-bt', 'bt-lr', 'rl-bt', 'bt-rl']
         self.orientation = "lr-tb"
-        for i in range(10):
+        for i in range(100):
             size = dp(100)
             b = Button(text = str(i+1), size_hint = (None, None), size=(size, size))
             self.add_widget(b)
