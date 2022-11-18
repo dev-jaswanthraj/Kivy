d16548fca4079e4c93c45800452635620218fb70
Jaswanthraj R <jaswanthraj.r@gmail.com>
2022-11-18 14:07:57 +0530

    Toggle

--- Basic Example/Widgets/main.py
+++ Basic Example/Widgets/main.py
@@ -1,16 +1,26 @@
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.gridlayout import GridLayout
 
 class WidgetExample(GridLayout):
     my_text = StringProperty("Hello !")
     count = StringProperty('0')
+    state = BooleanProperty(False)
     
     def on_press_event(self):
         self.my_text = "You Clicked!"
     
     def increment(self):
-        self.count = str(int(self.count)+1)
+        if self.state:
+            self.count = str(int(self.count)+1)
+    
+    def toggle_on(self, toggle_button):  
+        if toggle_button.state == "down":
+            toggle_button.text = "ON"
+            self.state = True
+        else:
+            self.state = False
+            toggle_button.text = "OFF"
     
 
 class Widgets(App):
